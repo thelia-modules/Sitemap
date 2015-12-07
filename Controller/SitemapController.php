f29dae89ffbcbf543698476bf4eeb594b77181a2
Etienne Perriere <eperriere@openstudio.fr>
2015-12-07 15:28:26 +0100

    Handle images

--- Controller/SitemapController.php
+++ Controller/SitemapController.php
@@ -6,14 +6,28 @@ use Doctrine\Common\Cache\FilesystemCache;
 use Propel\Runtime\ActiveQuery\Criteria;
 use Propel\Runtime\ActiveQuery\Join;
 use Thelia\Controller\Front\BaseFrontController;
+use Thelia\Core\Event\Image\ImageEvent;
+use Thelia\Core\Event\TheliaEvents;
 use Thelia\Core\HttpFoundation\Response;
+use Thelia\Model\CategoryI18nQuery;
+use Thelia\Model\CategoryImageQuery;
 use Thelia\Model\ConfigQuery;
+use Thelia\Model\ContentI18nQuery;
+use Thelia\Model\ContentImageQuery;
+use Thelia\Model\FolderI18nQuery;
+use Thelia\Model\FolderImageQuery;
 use Thelia\Model\LangQuery;
+use Thelia\Model\Map\CategoryI18nTableMap;
 use Thelia\Model\Map\CategoryTableMap;
+use Thelia\Model\Map\ContentI18nTableMap;
 use Thelia\Model\Map\ContentTableMap;
+use Thelia\Model\Map\FolderI18nTableMap;
 use Thelia\Model\Map\FolderTableMap;
+use Thelia\Model\Map\ProductI18nTableMap;
 use Thelia\Model\Map\ProductTableMap;
 use Thelia\Model\Map\RewritingUrlTableMap;
+use Thelia\Model\ProductI18nQuery;
+use Thelia\Model\ProductImageQuery;
 use Thelia\Model\RewritingUrl;
 use Thelia\Model\RewritingUrlQuery;
 use Thelia\Tools\URL;
@@ -95,7 +109,8 @@ class SitemapController extends BaseFrontController
         $sitemap = ['<?xml version="1.0" encoding="UTF-8"?>
         <!-- Generated on : '. date('Y-m-d H:i:s') .' -->
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+        xmlns:xhtml="http://www.w3.org/1999/xhtml"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
             <url>
                 <loc>'.URL::getInstance()->getIndexPage().'</loc>
             </url>'
@@ -139,14 +154,34 @@ class SitemapController extends BaseFrontController
         // Execute query
         $results = $query->find();
 
-        // For each result, use URL and update to hydrate XML file
+        // For each result, use URL, update and image to hydrate XML file
         /** @var RewritingUrl $result */
         foreach ($results as $result) {
+
+            // Open new sitemap line & set category URL & update date
             $sitemap[] = '
             <url>
                 <loc>'.URL::getInstance()->absoluteUrl($result->getUrl()).'</loc>
-                <lastmod>'.$result->getVirtualColumn('CATEGORY_UPDATE_AT').'</lastmod>
-            </url>';
+                <lastmod>'.$result->getVirtualColumn('CATEGORY_UPDATE_AT').'</lastmod>';
+
+            // Handle category image
+            $image = CategoryImageQuery::create()
+                ->filterByCategoryId($result->getViewId())
+                ->orderByPosition(Criteria::ASC)
+                ->findOne();
+
+            if ($image !== null) {
+                $title = CategoryI18nQuery::create()
+                    ->filterById($result->getViewId())
+                    ->filterByLocale($locale)
+                    ->select(CategoryI18nTableMap::TITLE)
+                    ->findOne();
+
+                $this->generateSitemapImage('category', $image, $title, $sitemap);
+            }
+
+            // Close category line
+            $sitemap[] = '            </url>';
         }
     }
 
@@ -204,11 +239,31 @@ class SitemapController extends BaseFrontController
         // For each result, use URL and update to hydrate XML file
         /** @var RewritingUrl $result */
         foreach ($results as $result) {
+
+            // Open new sitemap line & set product URL & update date
             $sitemap[] = '
             <url>
                 <loc>'.URL::getInstance()->absoluteUrl($result->getUrl()).'</loc>
-                <lastmod>'.$result->getVirtualColumn('PRODUCT_UPDATE_AT').'</lastmod>
-            </url>';
+                <lastmod>'.$result->getVirtualColumn('PRODUCT_UPDATE_AT').'</lastmod>';
+
+            // Handle product image
+            $image = ProductImageQuery::create()
+                ->filterByProductId($result->getViewId())
+                ->orderByPosition(Criteria::ASC)
+                ->findOne();
+
+            if ($image !== null) {
+                $title = ProductI18nQuery::create()
+                    ->filterById($result->getViewId())
+                    ->filterByLocale($locale)
+                    ->select(ProductI18nTableMap::TITLE)
+                    ->findOne();
+
+                $this->generateSitemapImage('product', $image, $title, $sitemap);
+            }
+
+            // Close product line
+            $sitemap[] = '            </url>';
         }
     }
 
@@ -266,11 +321,31 @@ class SitemapController extends BaseFrontController
         // For each result, use URL and update to hydrate XML file
         /** @var RewritingUrl $result */
         foreach ($results as $result) {
+
+            // Open new sitemap line & set folder URL & update date
             $sitemap[] = '
             <url>
                 <loc>'.URL::getInstance()->absoluteUrl($result->getUrl()).'</loc>
-                <lastmod>'.$result->getVirtualColumn('FOLDER_UPDATE_AT').'</lastmod>
-            </url>';
+                <lastmod>'.$result->getVirtualColumn('FOLDER_UPDATE_AT').'</lastmod>';
+
+            // Handle folder image
+            $image = FolderImageQuery::create()
+                ->filterByFolderId($result->getViewId())
+                ->orderByPosition(Criteria::ASC)
+                ->findOne();
+
+            if ($image !== null) {
+                $title = FolderI18nQuery::create()
+                    ->filterById($result->getViewId())
+                    ->filterByLocale($locale)
+                    ->select(FolderI18nTableMap::TITLE)
+                    ->findOne();
+
+                $this->generateSitemapImage('folder', $image, $title, $sitemap);
+            }
+
+            // Close folder line
+            $sitemap[] = '            </url>';
         }
     }
 
@@ -328,11 +403,31 @@ class SitemapController extends BaseFrontController
         // For each result, use URL and update to hydrate XML file
         /** @var RewritingUrl $result */
         foreach ($results as $result) {
+
+            // Open new sitemap line & set content URL & update date
             $sitemap[] = '
             <url>
                 <loc>'.URL::getInstance()->absoluteUrl($result->getUrl()).'</loc>
-                <lastmod>'.$result->getVirtualColumn('CONTENT_UPDATE_AT').'</lastmod>
-            </url>';
+                <lastmod>'.$result->getVirtualColumn('CONTENT_UPDATE_AT').'</lastmod>';
+
+            // Handle content image
+            $image = ContentImageQuery::create()
+                ->filterByContentId($result->getViewId())
+                ->orderByPosition(Criteria::ASC)
+                ->findOne();
+
+            if ($image !== null) {
+                $title = ContentI18nQuery::create()
+                    ->filterById($result->getViewId())
+                    ->filterByLocale($locale)
+                    ->select(ContentI18nTableMap::TITLE)
+                    ->findOne();
+
+                $this->generateSitemapImage('content', $image, $title, $sitemap);
+            }
+
+            // Close folder line
+            $sitemap[] = '            </url>';
         }
     }
 
@@ -364,6 +459,48 @@ class SitemapController extends BaseFrontController
     }
 
     /* ------------------ */
+
+    /**
+     * @param $type
+     * @param $image
+     * @param $title
+     * @param $sitemap
+     */
+    protected function generateSitemapImage($type, $image, $title, &$sitemap)
+    {
+        $event = new ImageEvent();
+
+        $event
+            ->setWidth(560)
+            ->setHeight(408)
+            ->setQuality(10)
+            ->setRotation(0)
+            ->setResizeMode(\Thelia\Action\Image::EXACT_RATIO_WITH_BORDERS);
+
+        // Put source image file path
+        $source_filepath = sprintf("%s%s/%s/%s",
+            THELIA_ROOT,
+            ConfigQuery::read('images_library_path', 'local/media/images'),
+            $type,
+            $image->getFile()
+        );
+
+        $event->setSourceFilepath($source_filepath);
+        $event->setCacheSubdirectory($type);
+
+        try {
+            // Dispatch image processing event
+            $this->dispatch(TheliaEvents::IMAGE_PROCESS, $event);
+        } catch (\Exception $ex) {
+        }
+
+        // Set image path in the sitemap file
+        $sitemap[] = '
+                <image:image>
+                    <image:loc>'.$event->getFileUrl().'</image:loc>
+                    <image:title>'.$title.'</image:title>
+                </image:image>';
+    }
 
     /**
      * @param $locale
