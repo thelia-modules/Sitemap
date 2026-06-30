a15dd6cd57a079a524de0f75b210ebbdc2e198d4
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-30 12:34:01 +0200

    fix: use correct i18n domain for sitemap config labels

--- I18n/backOffice/default/en_US.php
+++ I18n/backOffice/default/en_US.php
@@ -2,11 +2,15 @@
 
 return array(
     '<b>Warning!</b> Only fill this input with a greater value than 30 if you have so many images that the sitemap-image can\'t be generated because of timeout.' => '<b>Warning!</b> Only fill this input with a greater value than 30 if you have so many images that the sitemap-image can\'t be generated because of timeout.',
+    'Cancel' => 'Cancel',
     'Configuration correctly saved' => 'Configuration correctly saved',
+    'Configure sitemap' => 'Configure sitemap',
     'Configure sitemap images' => 'Configure sitemap images',
+    'Default priorities' => 'Default priorities',
     'Depending on your server, this may have no effect.' => 'Depending on your server, this may have no effect.',
     'Home' => 'Home',
     'Modules' => 'Modules',
+    'Save' => 'Save',
     'Set the same information as in your product image loop' => 'Set the same information as in your product image loop',
     'Sitemap images configuration' => 'Sitemap images configuration',
 );
--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -2,12 +2,15 @@
 
 return array(
     '<b>Warning!</b> Only fill this input with a greater value than 30 if you have so many images that the sitemap-image can\'t be generated because of timeout.' => '<b>Attention !</b> Ne remplissez ce champ avec une valeur plus grande que 30 que si vous avez beaucoup d\'images et que le sitemap-image n\'arrive pas à se générer.',
+    'Cancel' => 'Annuler',
     'Configuration correctly saved' => 'Configuration sauvegardée avec succès',
     'Configure sitemap' => 'Configurer le sitemap',
     'Configure sitemap images' => 'Configurer les images du sitemap',
+    'Default priorities' => 'Priorités par défaut',
     'Depending on your server, this may have no effect.' => 'En fonction de votre serveur, cela pourrait n\'avoir aucun effet.',
     'Home' => 'Accueil',
     'Modules' => 'Modules',
+    'Save' => 'Enregistrer',
     'Set the same information as in your product image loop' => 'Entrez les mêmes informations que dans la boucle image des produits',
     'Sitemap images configuration' => 'Configuration des images du module Sitemap',
 );
